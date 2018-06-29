6a89e953e5fc9fc0cdbd177135ce9224658fe862
Tomasz Słabiak <slabiak.tomasz@gmail.com>
2018-06-29 05:21:54 +0200

    hasu ser password before save to db

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
 
+var {authenticate} = require('./middleware/authenticate');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -26,7 +27,7 @@ res.send(doc);
 });
 
 app.get('/todos', (req, res)=>{
-    Todo.find().then((todos)=>{
+    Todo.find().then((todos)=>{ss
         res.send({todos});
     }, (e)=>{
         res.send('cos poszlo nie tak');
@@ -97,6 +98,26 @@ app.patch('/todos/:id', (req, res)=>{
 
 });
 
+app.post('/users', (req, res) =>{
+    var body = _.pick(req.body, ['email','password']);
+
+    var user = new User(
+        body
+    );
+    
+    user.save().then(()=>{
+        return user.generateAuthToken();
+    
+    }).then((token)=> {
+        res.header('x-auth', token).send(user)
+    }).catch((e)=>{
+        res.status(400).send(e);
+    });
+    });
+
+app.get('/users/me', authenticate, (req,res)=>{
+   res.send(req.user);
+});
 
 app.listen(port, ()=>{
     console.log('started on port ' +port);
